e12a6eee7f0ed2485a5e6b80bfc04fffdc5dd1c6
jaywcjlove <398188662@qq.com>
2015-09-12 23:04:21 +0800

    Command line help with optimization.

--- .bin/idoc.js
+++ .bin/idoc.js
@@ -4,7 +4,7 @@ var commander = require('commander');
 var appInfo = require('../package');
 var idoc  = require('..');
 var log =console.log;
-// require('colorful').toxic()
+require('colorful').toxic()
 
 commander
     .usage('[options]')
@@ -18,11 +18,28 @@ commander
 
 commander
     .on('--help',function(){
-        // 图片文字 http://ascii.mastervb.net/text_to_ascii.php
+        log('  Examples:'.bold.blue);
+        log('')
+        log('    $ idoc init');
+        log('    $ idoc init [path] ');
+        log('    $ idoc init [path] -C ~/idoc/');
+
         
-        log('    █░░░█ █▀▀ ░░▀');
-        log('    █▄█▄█ █░░ ░░█');
-        log('    ░▀░▀░ ▀▀▀ █▄█');
+        // 图片文字 http://patorjk.com/software/taag/#p=testall&f=Graffiti&t=idoc
+
+        log('')
+        log('')
+        log('')
+        log('')
+        log("    ,,        ,,                    ")
+        log("    db      `7MM                    ")
+        log("              MM                    ")
+        log("  `7MM   ,M\"\"bMM  ,pW\"Wq.   ,p6\"bo  ")
+        log("    MM ,AP    MM 6W'   `Wb 6M'  OO  ")
+        log("    MM 8MI    MM 8M     M8 8M       ")
+        log("    MM `Mb    MM YA.   ,A9 YM.    , ")
+        log("  .JMML.`Wbmd\"MML.`Ybmd9'   YMbmd'  ")
+        log('')
         log('')
 
     })
