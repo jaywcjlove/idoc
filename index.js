e7ed173bfc9b1dae06d1f62b7a7540bce41ac986
jaywcjlove <398188662@qq.com>
2015-09-27 02:33:53 +0800

    Initializes the judgment.

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var init = require('./lib/init');
-var build = require('./lib/build');
-var watch = require('./lib/watch');
-var server = require('./lib/server');
+var init    = require('./lib/init');
+var build   = require('./lib/build');
+var watch   = require('./lib/watch');
+var server  = require('./lib/server');
+var color   = require('colorful');
+var path    = require('path');
+var file    = require('./lib/file');
 
 module.exports = function(commander){
+    var pkgurl = path.resolve('package.json');
 
-    if (commander.init) {
-        init(commander);
+    if (commander.init) init(commander);
+
+    if(!file.exists(pkgurl)){
+        return console.log( color.red('\n Please initialization! Please run "idoc build" command! \n') );
     }
     if(commander.build){
         build(commander);
