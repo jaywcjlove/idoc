4bee203949d301815b161273a0a275e6cec02549
jaywcjlove <398188662@qq.com>
2022-04-17 16:26:18 +0800

    fix: Fix markdown content href issue.

--- src/markdown/rehype-urls.ts
+++ src/markdown/rehype-urls.ts
@@ -3,13 +3,14 @@ import { Root, RootContent } from 'hast';
 const isAbsoluteURL = (str: string) => /^[a-z][a-z0-9+.-]*:/.test(str);
 
 export default function rehypeUrls(node: Root | RootContent) {
-  if (node.type === 'element' && node.properties.href && /.md$/.test(node.properties.href as string)) {
+  if (node.type === 'element' && node.properties.href && /.md/.test(node.properties.href as string)) {
     const href = node.properties.href as string;
     if (!isAbsoluteURL(href)) {
       if (/readme\.(md|markdown)$/i.test(href)) {
-        node.properties.href = href.toLocaleLowerCase().replace(/readme\.(md|markdown)$/i, 'index.html');
+        node.properties.href = href.toLocaleLowerCase().replace(/readme\.(md|markdown)/gi, 'index.html');
       } else {
-        node.properties.href = href.toLocaleLowerCase().replace(/\.(md|markdown)$/, '.html');
+        console.log('href', node, href, href.replace(/([^\.\/\\]+)\.(md|markdown)/gi, '$1.html'));
+        node.properties.href = href.toLocaleLowerCase().replace(/([^\.\/\\]+)\.(md|markdown)/gi, '$1.html');
       }
     }
   }
